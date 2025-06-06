831d7550719ed59de86b1384ee2f40990d4d8276
rtatlisu <robin.tatlisu@web.de>
2025-06-06 21:25:11 +0200

    rewrote the game in OOP

--- game.py
+++ game.py
@@ -1,206 +1,191 @@
 import pygame
 import random
-from helperClass import getDangers, getFruitPosition, getDirection
+from helperClass import get_dangers, get_fruit_position, get_direction
 
+pygame.init()
+font = pygame.font.SysFont(None, 25)
 
 SCREEN_SIZE = 640
 WHITE = (128,128,128)
 RED = (255,0,0)
 BLACK = (0,0,0)
 CELLSIZE = 20
-ticks = 5
-tick_modifier = 1
-bodyPositions = []
-# initially moves to the right
-lastPressedKey = 'd'
-moveDir = 'right'
-fruitPos = None
-pause = False
 
+# used to easily store positions of snake segments and fruit
+class Vector2:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
 
-def main():
-    global screen, lastPressedKey, ticks, tick_modifier, pause
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_SIZE,SCREEN_SIZE))
-    clock = pygame.time.Clock()
-    running = True
+class SnakeAI:
+    def __init__(self, width, height, ticks):
+        self.width = width
+        self.height = height
+        self.ticks = ticks
+        self.tick_modifier = 1
+        self.fruit = None
+        self.lastPressedKey = 'd'
+        self.moveDir = 'right'
+        self.score = 0
 
+        self.head = Vector2(CELLSIZE*4,SCREEN_SIZE//2)
+        self.tail = Vector2(CELLSIZE*3, SCREEN_SIZE//2)
+        self.snake_segments = [self.head, self.tail]
 
-    while running:
+        self.fruit = self.spawn_fruit()
+
+        self.screen = pygame.display.set_mode((SCREEN_SIZE,SCREEN_SIZE))
+        self.clock = pygame.time.Clock()
+    
+    def gameLoop(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key in [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d]:
-                    pressed = pygame.key.name(event.key)
+                    self.pressed = pygame.key.name(event.key)
                     # makes sure that snake can't do a 180
-                    if (moveDir == 'up' and pressed != 's' or 
-                        moveDir == 'down' and pressed != 'w' or
-                        moveDir == 'left' and pressed != 'd' or 
-                        moveDir == 'right' and pressed != 'a'):
-                        lastPressedKey = pressed
+                    if (self.moveDir == 'up' and self.pressed != 's' or 
+                        self.moveDir == 'down' and self.pressed != 'w' or
+                        self.moveDir == 'left' and self.pressed != 'd' or 
+                        self.moveDir == 'right' and self.pressed != 'a'):
+                        self.lastPressedKey = self.pressed
                 # speeds up game
                 elif event.key == pygame.K_UP:
-                    tick_modifier += 0.2
+                    self.tick_modifier += 0.2
                 # slows down game
                 elif event.key == pygame.K_DOWN:
-                    if ticks * (tick_modifier - 0.2) > 1:
-                        tick_modifier -= 0.2
-                # pauses game
-                elif event.key == pygame.K_SPACE:
-                    pause = not pause
-                    
+                    if self.ticks * (self.tick_modifier - 0.2) > 1:
+                        self.tick_modifier -= 0.2
 
 
-        #drawGrid()
-        if(len(bodyPositions) > 0 and not pause):
-            move(lastPressedKey)
-            if fruitCollision():
-                pass
-            elif wallCollision():
-                continue
-            elif tailCollision():
-                continue
+        self.move(self.lastPressedKey)
+        # moving the snake consists of adding a 'cube' in front of the head and removing the tail
+        self.snake_segments.insert(0, self.head)
+
+        # if death by either, reset game and exit current loop iteration
+        if self.is_tail_collision() or self.is_wall_collision():
+            self.death()
+            return
+        # if fruit eaten, dont remove last tail part 
+        if self.is_fruit_eaten():
+            self.fruit = self.spawn_fruit()
+        else:
+            self.snake_segments.pop()
+
         
-        screen.fill(BLACK)
-        drawSnake()
-        drawFruit()
-        if(len(bodyPositions) > 0 ):
-            #print(getDangers(bodyPositions, moveDir, CELLSIZE, SCREEN_SIZE))
-            #print(getFruitPosition((bodyPositions[0].x,bodyPositions[0].y), (fruitPos.x,fruitPos.y)))
-            print(getDirection(moveDir))
+        self.update_ui()
+        # tick_modifier used to increase game speed while in-game
+        self.clock.tick(self.ticks * self.tick_modifier)
+
+
+
+
+    def update_ui(self):
+        self.screen.fill(BLACK)
+
+        # draw snake
+        for el in self.snake_segments:
+            rect = pygame.Rect(el.x, el.y, CELLSIZE-2,CELLSIZE-2)
+            pygame.draw.rect(self.screen, WHITE, rect, 10)
+
+        # draw fruit
+        rect = pygame.Rect(self.fruit.x, self.fruit.y, CELLSIZE-2,CELLSIZE-2)
+        pygame.draw.rect(self.screen, RED, rect, 10)
+
+        # other
+        text_surface = font.render(f'Score: {self.score}', True, (255, 255, 255))
+        self.screen.blit(text_surface, (10, 10))
 
         pygame.display.update()
-        clock.tick(ticks * tick_modifier)
+
+    # for visualization purposes regarding cells
+    def drawGrid(self):
+        for i in range(0, SCREEN_SIZE, CELLSIZE):
+            for j in range(0, SCREEN_SIZE, CELLSIZE):
+                rect = pygame.Rect(i, j, CELLSIZE, CELLSIZE)
+                pygame.draw.rect(self.screen, WHITE, rect, 1)
+                pygame.draw.circle(self.screen,RED,[i+CELLSIZE/2,j+CELLSIZE/2],1)
 
 
+    def move(self, direction):
+        xPos = self.head.x
+        yPos = self.head.y
 
-    pygame.quit()
-
-
-# for visualization purposes regarding cells
-def drawGrid():
-    for i in range(0, SCREEN_SIZE, CELLSIZE):
-        for j in range(0, SCREEN_SIZE, CELLSIZE):
-            rect = pygame.Rect(i, j, CELLSIZE, CELLSIZE)
-            pygame.draw.rect(screen, WHITE, rect, 1)
-            pygame.draw.circle(screen,RED,[i+CELLSIZE/2,j+CELLSIZE/2],1)
-
-
-def drawSnake():
-    global bodyPositions
-    # initial drawing/spawning of the snake
-    if len(bodyPositions) == 0:
-        head = pygame.Rect(CELLSIZE*4,SCREEN_SIZE/2, CELLSIZE-2, CELLSIZE-2)
-        pygame.draw.rect(screen, WHITE, head, 10)
-        tail = pygame.Rect(head.left-CELLSIZE,head.top, CELLSIZE-2, CELLSIZE-2)
-        pygame.draw.rect(screen, WHITE, tail, 10)
-        bodyPositions = [head,tail]
-    else:
-        # draw tails
-        for i in range(1, len(bodyPositions)):
-            rect = pygame.Rect(bodyPositions[i].left,bodyPositions[i].top, CELLSIZE-2,CELLSIZE-2)
-            pygame.draw.rect(screen, WHITE, rect, 10)
-        # draw head
-        rect = pygame.Rect(bodyPositions[0].left,bodyPositions[0].top, CELLSIZE-2,CELLSIZE-2)
-        pygame.draw.rect(screen, WHITE, rect, 10)
-
-
-
-def move(key):
-    global moveDir
-    # starting from the last part of the snake's tail, assign the predecessor's position
-    # done for all parts but the head
-    for i in range(-1, -len(bodyPositions),-1):
-        bodyPositions[i].x = bodyPositions[i-1].x
-        bodyPositions[i].y = bodyPositions[i-1].y
+        if direction == 'w':
+            yPos -= CELLSIZE
+            self.moveDir = 'up'
+        elif direction == 'a':
+            xPos -= CELLSIZE
+            self.moveDir = 'left'
+        elif direction == 's':
+            yPos += CELLSIZE
+            self.moveDir = 'down'
+        elif direction == 'd':
+            xPos += CELLSIZE
+            self.moveDir = 'right'
         
+        # create new instance for self.head to insert at the top of list and after pop the last element in list
+        self.head = Vector2(xPos,yPos)
 
-    # separately updates the head's position from the rest of its body
-    if key == 'w':
-        bodyPositions[0].y -= CELLSIZE
-        moveDir = 'up'
-    elif key == 'a':
-        bodyPositions[0].x -= CELLSIZE
-        moveDir = 'left'
-    elif key == 's':
-        bodyPositions[0].y += CELLSIZE
-        moveDir = 'down'
-    elif key == 'd':
-        bodyPositions[0].x += CELLSIZE
-        moveDir = 'right'
-
-
-# spawns the fruit and makes sure that it's not spawning on top of the snake
-def drawFruit():
-    global fruitPos
-    # random numbers between 0 - 31 and then multiplying by CELLSIZE to assign fruit to a cell
-    # -2 comes from design choice, since the snake shouldn't be a solid moving mass, but visually separable and
-    # therefore the fruit should have the same size
-    if fruitPos == None:     
+  
+    # spawns the fruit and makes sure that it's not spawning on top of the snake
+    def spawn_fruit(self) -> Vector2:
+        # random numbers between 0 - 31 and then multiplying by CELLSIZE to assign fruit to a cell
+        # -2 comes from design choice, since the snake shouldn't be a solid moving mass, but visually separable and
+        # therefore the fruit should have the same size   
         xPos = random.randint(0,31)
         yPos = random.randint(0,31)
         xPos *= CELLSIZE 
         yPos *= CELLSIZE
-    else:
-        xPos = fruitPos.x
-        yPos = fruitPos.y
 
-    rect = pygame.Rect(xPos,yPos, CELLSIZE-2,CELLSIZE-2)
-    pygame.draw.rect(screen, RED, rect, 10)
-    fruitPos = rect
+        return Vector2(xPos,yPos)
 
-
-def wallCollision():
     # check if snake runs into wall
-    head = bodyPositions[0]
-    if head.x + CELLSIZE > SCREEN_SIZE or head.x < 0 or head.y + CELLSIZE > SCREEN_SIZE  or head.y < 0:
-        death()
-        return True
-    return False
-
-
-def fruitCollision():
-    global fruitPos
-    # check if fruit was eaten
-    try:
-        if bodyPositions[0].colliderect(fruitPos):
-            # add tail to end of  snake
-            rect = pygame.Rect(bodyPositions[-1].left,bodyPositions[-1].top, CELLSIZE-2,CELLSIZE-2)
-            pygame.draw.rect(screen, WHITE, rect, 10)
-            bodyPositions.append(rect)
-            fruitPos = None
+    def is_wall_collision(self):
+        if (self.head.x + CELLSIZE > SCREEN_SIZE or self.head.x < 0 or 
+            self.head.y + CELLSIZE > SCREEN_SIZE  or self.head.y < 0):
             return True
         return False
 
-    except Exception as e:
-        print('couldn\'t determine if fruit was eaten')
-        print(e)
-
-
-def tailCollision():
-    head = bodyPositions[0]
-    for tail in bodyPositions[1:]:
-        if head.colliderect(tail):
-            death()
+    # check if fruit was eaten
+    def is_fruit_eaten(self):
+        if self.head.x == self.fruit.x and self.head.y == self.fruit.y:
+            self.score += 1
             return True
-    return False
+        return False
+      
 
 
-# resets runtime variables to initial state for restart
-def death():
-    global bodyPositions, fruitPos, lastPressedKey, moveDir
-    bodyPositions = []
-    fruitPos = None
-    lastPressedKey = 'd'
-    moveDir = 'right'
+    def is_tail_collision(self):
+        for el in self.snake_segments[1:]:
+            if self.head.x == el.x and self.head.y == el.y:
+                return True
+        return False
 
 
 
-    
+    # resets runtime variables to initial state for restart
+    def death(self):
+        self.snake_segments = []
+        self.fruit = None
+        self.lastPressedKey = 'd'
+        self.moveDir = 'right'
+        self.score = 0
+
+        self.head = Vector2(CELLSIZE*4,SCREEN_SIZE//2)
+        self.tail = Vector2(CELLSIZE*3, SCREEN_SIZE//2)
+        self.snake_segments = [self.head, self.tail]
+
+        self.fruit = self.spawn_fruit()
 
 
-    
+
+        
+
 
 if __name__ == '__main__':
-    main()
+    instance = SnakeAI(640,640,5)
+
+    while True:
+        instance.gameLoop()
--- helperClass.py
+++ helperClass.py
@@ -2,13 +2,13 @@ import pygame
 
 # returns a 3-element binary list indicating if there is a danger to the left, straight or right relative to
 # the snake's orentation
-def getDangers(
-        bodyPositions: list[pygame.Rect], 
+def get_dangers(
+        snake_segments: list[pygame.Rect], 
         direction: str,
         CELLSIZE: int, 
         SCREEN_SIZE: int
         ):
-    head = bodyPositions[0]
+    head = snake_segments[0]
     dangers = []
 
     head_next_up = pygame.Rect(head)
@@ -19,65 +19,65 @@ def getDangers(
     head_next_down.y += CELLSIZE
     head_next_left.x -= CELLSIZE
     head_next_right.x += CELLSIZE
-    northTail = False
-    eastTail = False
-    southTail = False
-    westTail = False
+    north_tail = False
+    east_tail = False
+    south_tail = False
+    west_tail = False
 
-    for tail in bodyPositions[1:]:
+    for tail in snake_segments[1:]:
         if head_next_up.colliderect(tail):
-            northTail = True
+            north_tail = True
         elif head_next_right.colliderect(tail):
-            eastTail = True
+            east_tail = True
         elif head_next_down.colliderect(tail):
-            southTail = True
+            south_tail = True
         elif head_next_left.colliderect(tail):
-            westTail = True
+            west_tail = True
     
-    northWall = True if head.y - CELLSIZE < 0 else False
-    eastWall = True if head.x + CELLSIZE >= SCREEN_SIZE else False
-    southWall = True if head.y + CELLSIZE >= SCREEN_SIZE else False
-    westWall = True if head.x - CELLSIZE < 0 else False
+    north_wall = True if head.y - CELLSIZE < 0 else False
+    east_wall = True if head.x + CELLSIZE >= SCREEN_SIZE else False
+    south_wall = True if head.y + CELLSIZE >= SCREEN_SIZE else False
+    west_wall = True if head.x - CELLSIZE < 0 else False
 
     # order: straight, left, right
     if direction == 'up':
-        dangers.append(1 if northWall or northTail else 0)
-        dangers.append(1 if westWall or westTail else 0)
-        dangers.append(1 if eastWall or eastTail else 0)
+        dangers.append(1 if north_wall or north_tail else 0)
+        dangers.append(1 if west_wall or west_tail else 0)
+        dangers.append(1 if east_wall or east_tail else 0)
     elif direction == 'down':
-        dangers.append(1 if southWall or southTail else 0)
-        dangers.append(1 if eastWall or eastTail else 0)
-        dangers.append(1 if westWall or westTail else 0)
+        dangers.append(1 if south_wall or south_tail else 0)
+        dangers.append(1 if east_wall or east_tail else 0)
+        dangers.append(1 if west_wall or west_tail else 0)
     elif direction == 'left':
-        dangers.append(1 if westWall or westTail else 0)
-        dangers.append(1 if southWall or southTail else 0)
-        dangers.append(1 if northWall or northTail else 0)
+        dangers.append(1 if west_wall or west_tail else 0)
+        dangers.append(1 if south_wall or south_tail else 0)
+        dangers.append(1 if north_wall or north_tail else 0)
     else:
-        dangers.append(1 if eastWall or eastTail else 0)
-        dangers.append(1 if northWall or northTail else 0)
-        dangers.append(1 if southWall or southTail else 0)
+        dangers.append(1 if east_wall or east_tail else 0)
+        dangers.append(1 if north_wall or north_tail else 0)
+        dangers.append(1 if south_wall or south_tail else 0)
     
 
     return dangers
 
 
 # returns a 4-element binary list giving info on the fruit's position, e.g. up right
-def getFruitPosition(headPos: tuple[int,int], fruitPos: tuple[int,int]):
-    head_x, head_y = headPos
-    fruit_x, fruit_y = fruitPos
+def get_fruit_position(head_pos: tuple[int,int], fruit_pos: tuple[int,int]):
+    head_x, head_y = head_pos
+    fruit_x, fruit_y = fruit_pos
 
     # order: up, left, right, down
-    fruitDirection = []
-    fruitDirection.append(1 if head_y - fruit_y > 0 else 0)
-    fruitDirection.append(1 if head_x - fruit_x > 0 else 0)
-    fruitDirection.append(1 if head_x - fruit_x < 0 else 0)
-    fruitDirection.append(1 if head_y - fruit_y < 0 else 0)
+    fruit_direction = []
+    fruit_direction.append(1 if head_y - fruit_y > 0 else 0)
+    fruit_direction.append(1 if head_x - fruit_x > 0 else 0)
+    fruit_direction.append(1 if head_x - fruit_x < 0 else 0)
+    fruit_direction.append(1 if head_y - fruit_y < 0 else 0)
 
-    return fruitDirection
+    return fruit_direction
 
 
 # returns 4-element binary list with direction of snake
-def getDirection(direction: str):
+def get_direction(direction: str):
     # order: up, left, right, dwon
     return [
         1 if direction == 'up' else 0,
